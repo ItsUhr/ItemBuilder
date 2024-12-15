64b38cabf6d94398700f4fe29acd8e796c43ec94
ItsUhr <148636607+ItsUhr@users.noreply.github.com>
2024-12-15 22:35:07 +0100

    Neuer  ItemBuilder.java
    Paper 1.21 Component Adventure API

--- ItemBuilder.java
+++ ItemBuilder.java
@@ -1,82 +1,100 @@
-package de.itsuhr.teamCastle.builder;
-
+import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemRarity;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 public class ItemBuilder {
 
-    private ItemStack itemStack;
-    private ItemMeta itemMeta;
+    private final ItemStack itemStack;
+    private final ItemMeta itemMeta;
 
-    public ItemBuilder(Material material) {
-        itemStack = new ItemStack(material);
-        itemMeta = itemStack.getItemMeta();
+    public ItemBuilder(@NotNull Material material) {
+        this.itemStack = new ItemStack(material);
+        this.itemMeta = itemStack.getItemMeta();
     }
 
-    public ItemBuilder(Material material, int amount) {
-        itemStack = new ItemStack(material, amount);
-        itemMeta = itemStack.getItemMeta();
+    public ItemBuilder(@NotNull Material material, int amount) {
+        this.itemStack = new ItemStack(material, amount);
+        this.itemMeta = itemStack.getItemMeta();
     }
 
-
-    public ItemBuilder(Material material, int amount, short damage) {
-        itemStack = new ItemStack(material, amount, damage);
-        itemMeta = itemStack.getItemMeta();
+    public ItemBuilder(@NotNull ItemStack stack) {
+        this.itemStack = new ItemStack(stack);
+        this.itemMeta = itemStack.getItemMeta();
     }
 
-    public ItemBuilder(Material material, int amount, short damage, Byte data) {
-        itemStack = new ItemStack(material, amount, damage, data);
-        itemMeta = itemStack.getItemMeta();
-    }
-
-    public ItemBuilder setDisplayName(String displayName) {
-        itemMeta.setDisplayName(displayName);
+    public ItemBuilder setDisplayName(@NotNull String displayName) {
+        this.itemMeta.displayName(Component.text(displayName));
         return this;
     }
 
-    public ItemBuilder setVersion(int version) {
-        itemMeta.setVersion(version);
+    public ItemBuilder getDisplayName() {
+        this.itemMeta.displayName();
         return this;
     }
 
-    public ItemBuilder setItemName(String itemName) {
-        itemMeta.setItemName(itemName);
+    public ItemBuilder setEnchant(@NotNull Enchantment ench, int level, boolean ignoreLevelRestriction) {
+        this.itemMeta.addEnchant(ench, level, ignoreLevelRestriction);
         return this;
     }
 
-    public ItemBuilder setLore(ArrayList<String> lore) {
-        itemMeta.setLore(lore);
+    public ItemBuilder getEnchants() {
+        this.itemMeta.getEnchants();
         return this;
     }
 
-    public ItemBuilder setLore(String lore) {
-        itemMeta.setLore(Arrays.asList(lore));
+    public ItemBuilder getEnchants(Enchantment ench) {
+        this.itemMeta.getEnchantLevel(ench);
+        return this;
+    }
+
+    public ItemBuilder setLore(@NotNull String lore) {
+        this.itemMeta.lore(List.of(Component.text(lore)));
+        return this;
+    }
+
+    public ItemBuilder getLore() {
+        this.itemMeta.lore();
         return this;
     }
 
     public ItemBuilder setUnbreakable(boolean unbreakable) {
-        itemMeta.setUnbreakable(unbreakable);
+        this.itemMeta.setUnbreakable(unbreakable);
         return this;
     }
 
-    public ItemBuilder setEnchant(Enchantment enchant, int level, boolean ignoreLevelRestriction) {
-        itemMeta.addEnchant(enchant, level, ignoreLevelRestriction);
+    public ItemBuilder setItemName(@NotNull String itemName) {
+        this.itemMeta.itemName(Component.text(itemName));
         return this;
     }
 
-    public ItemBuilder removeEnchant(Enchantment enchantment) {
-        itemMeta.removeEnchant(enchantment);
+    public ItemBuilder getItemName() {
+        this.itemMeta.itemName();
         return this;
     }
 
-    public ItemStack build() {
-        itemStack.setItemMeta(itemMeta);
-        return itemStack;
+    public ItemBuilder setCustomModelData(@NotNull Integer data) {
+        this.itemMeta.setCustomModelData(data);
+        return this;
     }
 
+    public ItemBuilder setRarity(@NotNull ItemRarity rarity) {
+        this.itemMeta.setRarity(rarity);
+        return this;
+    }
+
+    public ItemBuilder setFireResistant(boolean fireResistant) {
+        this.itemMeta.setFireResistant(fireResistant);
+        return this;
+    }
+
+    public ItemBuilder setHideTooltip(boolean hideTooltip) {
+        this.itemMeta.setHideTooltip(hideTooltip);
+        return this;
+    }
 }
